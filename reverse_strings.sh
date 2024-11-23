9a7497d2756e393204099628ff745af886d09d8b
KartheesP <kartheespskg@gmail.com>
2024-11-23 13:15:03 +0530

    Create reverse_strings.sh
    Adding reverse_strings.sh shell script

---
+++ reverse_strings.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+flag=$1
+input_string=$2
+
+if [ "$flag" != "-e" ] && [ "$flag" != "-w" ]; then
+echo "Invalid input. Use -e for entire reversal or -w for word reversal."
+exit 1
+
+fi
+
+if [ "$flag" == "-e" ]; then
+echo "$input_string" | rev
+
+elif [ "$flag" == "-w" ]; then
+
+for word in $input_string; do
+
+echo -n "$(echo $word | rev) "
+done
+
+echo
+fi
